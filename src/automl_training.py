38a9f9d2ead0c6ce5852656724af43d91b88a079
Geovany Ocaña <egoo.geovany@gmail.com>
2024-11-03 16:36:59 -0600

    Added Cross Validator and optimizations

--- src/automl_training.py
+++ src/automl_training.py
@@ -1,41 +1,39 @@
 import pandas as pd
-from tpot import TPOTRegressor
 from sklearn.metrics import mean_squared_error
 import joblib
-import sys
-import os
+from tpot import TPOTRegressor
 
 output_dir = "models"
-os.makedirs(output_dir, exist_ok=True)
 
-# Cargar datos de entrenamiento y prueba
-def load_data():
-    X_train = pd.read_csv("data/processed/X_train.csv")
-    X_test = pd.read_csv("data/processed/X_test.csv")
-    y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
-    y_test = pd.read_csv("data/processed/y_test.csv").values.ravel()
-    return X_train, X_test, y_train, y_test
-
-# Entrenar modelo utilizando TPOT
 def train_automl(X_train, y_train, X_test, y_test):
-    tpot = TPOTRegressor(verbosity=2, random_state=42, generations=5, population_size=20, config_dict='TPOT sparse')
+    tpot = TPOTRegressor(
+        verbosity=2,
+        random_state=42,
+        generations=5,
+        population_size=20,
+        config_dict='TPOT sparse',
+        cv=5  # validación cruzada
+    )
+    
     tpot.fit(X_train, y_train)
 
     # Predecir y calcular el error
     predictions = tpot.predict(X_test)
     mse = mean_squared_error(y_test, predictions)
     
-    # Guardar el mejor modelo
+    # guardamos el mejor modelo
     tpot.export(f"{output_dir}/best_model.py")
     joblib.dump(tpot.fitted_pipeline_, f"{output_dir}/best_model.joblib")
 
     return mse
 
 if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = load_data()
-    mse = train_automl(X_train, y_train, X_test, y_test)
+    # cargar datos
+    X_train = pd.read_csv("data/processed/X_train.csv")
+    X_test = pd.read_csv("data/processed/X_test.csv")
+    y_train = pd.read_csv("data/processed/y_train.csv").values.ravel()
+    y_test = pd.read_csv("data/processed/y_test.csv").values.ravel()
 
-    # Guardar resultados en un archivo de texto
-    with open(f"{output_dir}/automl_results.txt", "w") as f:
-        f.write(f"Mean Squared Error: {mse}\n")
-        f.write(f"Best pipeline exported as: best_model.py\n")
+    # entrenar y evaluar el modelo
+    mse = train_automl(X_train, y_train, X_test, y_test)
+    print(f'Mean Squared Error: {mse}')
